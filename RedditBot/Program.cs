bb65eed2fe4156164c4851286305085da78b4eba
daniel.olsson2 <daniel.olsson2@itggot.se>
2017-03-09 14:49:06 +0100

    skit
    bra skit

--- RedditBot/Program.cs
+++ RedditBot/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Configuration;
 
 namespace RedditBot
 {
@@ -10,7 +12,14 @@ namespace RedditBot
     {
         static void Main(string[] args)
         {
-            
+            RedditBot bot = new RedditBot("UltimateBottyBoi", "A very good bot", "1.0");
+            var clientId = ConfigurationManager.AppSettings["clientId"];
+            var clientSecret = ConfigurationManager.AppSettings["clientSecret"];
+            var username = ConfigurationManager.AppSettings["username"];
+            var password = ConfigurationManager.AppSettings["password"];
+            Console.WriteLine(bot.isAuthenticated());
+            bot.Authenticate(clientId, clientSecret, username, password);
+            Console.WriteLine(bot.isAuthenticated());
         }
     }
 }
---
+++ RedditBot/RedditBot.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RedditBot
+{
+    class RedditBot
+    {
+        private string _name;
+        private string _description;
+        private string _version;
+        private string _accessToken = "";
+
+        public RedditBot(string name, string description, string version)
+        {
+            _name = name;
+            _description = description;
+            _version = version;
+        }
+
+        public bool isAuthenticated()
+        {
+            if (_accessToken.Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Authenticate(string clientId, string clientSecret, string username, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                var authenticationArray = Encoding.ASCII.GetBytes($"{clientId}:{clientSecret}");
+                var encodedAuthenticationString = Convert.ToBase64String(authenticationArray);
+
+                client.DefaultRequestHeaders.Authorization = new
+                System.Net.Http.Headers.AuthenticationHeaderValue("Basic", encodedAuthenticationString);
+
+                client.DefaultRequestHeaders.Add("User-Agent", $"{_name} /v{_version} by {username}");
+
+                var formData = new Dictionary<string, string>
+                {
+                    { "grant_type", "password" },
+                    { "username", username },
+                    { "password", password }
+                };
+                var encodedFormData = new FormUrlEncodedContent(formData);
+
+                var authUrl = "https://www.reddit.com/api/v1/access_token";
+                var response = client.PostAsync(authUrl, encodedFormData).GetAwaiter().GetResult();
+
+                // Response Code
+                // Console.WriteLine(response.StatusCode);
+
+                if (response.StatusCode.ToString() == "OK") {
+                    // Actual Token
+                    var responseData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    _accessToken = JObject.Parse(responseData).SelectToken("access_token").ToString();
+                }
+
+                // Update AuthorizationHeader
+                //client.DefaultRequestHeaders.Authorization = new
+                //System.Net.Http.Headers.AuthenticationHeaderValue("bearer", _accessToken);
+            }
+        }
+    }
+}
